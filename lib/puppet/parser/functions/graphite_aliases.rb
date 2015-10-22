20685b178c8af4ba36182f8a3e16f2cc1c530bf0
Marc Fournier <marc.fournier@camptocamp.com>
2015-10-22 10:06:28 +0200

    move graphite_aliases() function to collectd public module

---
+++ lib/puppet/parser/functions/graphite_aliases.rb
@@ -0,0 +1,58 @@
+Puppet::Parser::Functions.newfunction(
+  :graphite_aliases, :type => :rvalue :doc => <<-EOS
+  Transforms values returned by query_facts() into a bunch of File resources
+  which can be fed to create_resources(). The goal is to maintain a subtree of
+  symlinks in graphite, offering a fact-based classification of nodes.
+
+  Example:
+    create_resources('file', graphite_aliases(
+      query_facts(
+        'Class[Collectd]', [
+          'ec2_instance_type',
+          'lsbdistcodename',
+          'role',
+          'server_family',
+          'server_classification']),
+        '/srv/carbon/whisper'
+      )
+    )
+
+    File <| name == '/srv/carbon/whisper/aliases' |> {
+      purge   => true,
+      recurse => true,
+      force   => true,
+    }
+EOS
+) do |args|
+  dirlist = []
+  resources = {}
+
+  prefix = args.pop
+
+  args.pop.each do |host_dots, facts|
+    host = host_dots.gsub(/\./, '_')
+    facts.each do |k,v|
+      fact_dir = "aliases/by-#{k}/#{v}".gsub(/\./, '_')
+      dirlist << fact_dir
+      resources["#{prefix}/#{fact_dir}/#{host}"] = {
+        "ensure" => "symlink",
+        "target" => "#{prefix}/collectd/#{host}",
+      }
+    end
+  end
+
+  basedirs = []
+  dirlist.uniq.each do |e|
+    dirs = []
+    e.split(/\//).each {|d| dirs << "#{dirs.last}/#{d}" }
+    basedirs << dirs
+  end
+
+  basedirs.flatten.uniq.each do |dir|
+    resources["#{prefix}#{dir}"] = {
+      "ensure" => "directory",
+    }
+  end
+
+  resources
+end
