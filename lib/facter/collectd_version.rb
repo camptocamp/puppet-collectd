a1ace98ebfc790f04ad5f7b72ae9e26eeb0f9e57
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-01-11 16:34:18 +0100

    Fix collectd_version fact
    In "5.7.0.git,", "git" doesn't match /[0-9a-h\.]*/

--- lib/facter/collectd_version.rb
+++ lib/facter/collectd_version.rb
@@ -1,7 +1,7 @@
 Facter.add("collectd_version") do
   setcode do
     output = %x{collectd -h 2>&1}
-    if $?.exitstatus and output.match(/collectd (\d+\.\d+\.\d+)[0-9a-h\.]*,/)
+    if $?.exitstatus and output.match(/collectd (\d+\.\d+\.\d+)[0-9a-z\.]*,/)
       $1
     else
       "0.0.0"
